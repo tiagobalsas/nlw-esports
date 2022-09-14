4c682a1ec6b00952b5d1853bf8b4cdc5b868272a
Tiago Santos <tiago881@gmail.com>
2022-09-14 11:39:33 -0300

    feat:add game names and ads

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -16,23 +16,57 @@ function App() {
       </h1>
 
       <div className='grid grid-cols-6 gap-6 mt-16'>
-        <a href=''>
+        <a href='' className='relative rounded-lg overflow-hidden'>
           <img src='/image1.png' />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>
+              League of Legends
+            </strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
         </a>
-        <a href=''>
+
+        <a href='' className='relative rounded-lg overflow-hidden'>
           <img src='/image2.png' />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Dota 2</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
         </a>
-        <a href=''>
+
+        <a href='' className='relative rounded-lg overflow-hidden'>
           <img src='/image3.png' />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>CS GO</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
         </a>
-        <a href=''>
+
+        <a href='' className='relative rounded-lg overflow-hidden'>
           <img src='/image4.png' />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>
+              World of warcraft
+            </strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
         </a>
-        <a href=''>
+
+        <a href='' className='relative rounded-lg overflow-hidden'>
           <img src='/image5.png' />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Apex Legends</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
         </a>
-        <a href=''>
+
+        <a href='' className='relative rounded-lg overflow-hidden'>
           <img src='/image6.png' />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Fortnite</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
         </a>
       </div>
     </div>
