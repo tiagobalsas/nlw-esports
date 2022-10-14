85afb3857fc6938713d0d87b47dbd87fe63f5d33
Tiago Santos <tiago881@gmail.com>
2022-10-14 16:01:03 -0300

    feat:add Loading

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,25 @@
 import { StatusBar } from 'react-native';
 
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+} from '@expo-google-fonts/inter';
+
+import { Home } from './src/screens/Home';
+
 import { Background } from './src/components/Background';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black,
+  });
   return (
     <Background>
       <StatusBar
@@ -10,6 +27,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
+      {fontsLoaded ? <Home /> : <Loading />}
     </Background>
   );
 }
